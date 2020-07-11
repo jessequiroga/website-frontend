9cc990c67a527bbe3bfcb6ecf4273bde47f81d57
David Wang <devtech324@gmail.com>
2020-07-11 09:45:28 +0200

    Initial commit

---
+++ src/app.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+interface AppProps {
+}
+
+interface AppState {
+}
+
+export default class App extends React.Component<AppProps, AppState> {
+    constructor(props: AppProps) {
+        super(props);
+
+    }
+
+
+    render() {
+        return (
+            <div>
+                Hello world!
+            </div>
+        );
+    }
+}
