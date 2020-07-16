3ce9911e550d5345fc32207059c55110ac00d18e
David Wang <devtech324@gmail.com>
2020-07-16 20:28:41 +0200

    Fix issue with horizontal overflow

--- src/pages/workshop/workshop-page.tsx
+++ src/pages/workshop/workshop-page.tsx
@@ -10,21 +10,36 @@ export class WorkshopPage extends React.Component<any, any> {
     render() {
         return (
             <div className="workshop-page-container">
-                <div className="workshop-page-title">DELAVNICE</div>
-                <div className="workshop-page-cards-container"></div>
-                <Grid container justify="center" spacing={8}>
-                    {WorkshopsConst.returnListOfWorkshops().map((workshop) => {
-                        return (
-                            <Grid item key={workshop.key}>
-                                <WorkshopCardComponent
-                                    title={workshop.title}
-                                    mentor={workshop.mentor}
-                                    contents={workshop.description}
-                                />
+                <div className="workshop-page-cards-container">
+                    <Grid
+                        container
+                        direction="column"
+                        alignItems="center"
+                        justify="center"
+                        style={{ minHeight: '100vh' }}
+                    >
+                        <Grid item>
+                            <Grid container>
+                                <div className="workshop-page-title">DELAVNICE</div>
                             </Grid>
-                        );
-                    })}
-                </Grid>
+                        </Grid>
+                        <Grid item>
+                            <Grid container direction="row">
+                                {WorkshopsConst.returnListOfWorkshops().map((workshop) => {
+                                    return (
+                                        <Grid item key={workshop.key} style={{ padding: '16px' }}>
+                                            <WorkshopCardComponent
+                                                title={workshop.title}
+                                                mentor={workshop.mentor}
+                                                contents={workshop.description}
+                                            />
+                                        </Grid>
+                                    );
+                                })}
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </div>
             </div>
         );
     }
